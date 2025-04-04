98fd9f949250921a5249218d02f866fdb3f09bb3
florencioeu <robson.fb10@gmail.com>
2025-04-04 10:58:09 -0300

    initial commit

--- Aula 11/ex008.js
+++ Aula 11/ex008.js
@@ -1,1 +1,6 @@
-console.log('O console funcionou corretamente!')
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+   console.log(`Você ultrappasou a velocidade permitida, MULTADO!`) 
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
