b37813db23b46af12aa1b387952cb59b8c482706
florencioeu <robson.fb10@gmail.com>
2025-07-03 23:43:58 -0300

    repetições

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1
+do {
+    console.log('tudo bem?')
+    c++
+} while (c <= 4)
+
+/*
+var c = 1
+while (c <= 4) {
+    console.log('tudo bem?')
+    c++
+}
+*/
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
